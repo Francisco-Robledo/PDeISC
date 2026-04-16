c327e13771e29b02f2210c7b0086e4bafa44920d
Francisco-Robledo <164202063+Francisco-Robledo@users.noreply.github.com>
2026-04-16 20:18:02 -0300

    Delete NJS2 directory

--- NJS2/tres/ejercicio3.js
+++
@@ -1,8 +0,0 @@
-import url from 'url';
-
-const direccion = new URL('http://localhost:3000/pagina?nombre=juan&edad=20');
-
-console.log('Host:', direccion.host);
-console.log('Path:', direccion.pathname);
-console.log('Query:', direccion.query);
-console.log('protocol:', direccion.protocol);
--- NJS2/uno/modulos/calculo.js
+++
@@ -1,8 +0,0 @@
-export const calculo = {
-    sumar(a, b) {
-        return a + b;
-    },
-    restar(a, b) {
-        return a - b;
-    }
-};
